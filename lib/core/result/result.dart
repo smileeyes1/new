5484621eb5d89761a4776b483143c23b877f96f8
smileeyes1 <smile.eyes1@gmail.com>
2026-06-10 23:04:55 +0300

    Update result.dart

--- lib/core/result/result.dart
+++ lib/core/result/result.dart
@@ -1,60 +1,65 @@
 import 'package:equatable/equatable.dart';
 
+/// BASE RESULT
 abstract class Result<T> extends Equatable {
   const Result();
 
-  @override
-  List<Object?> get props => [];
+  R when<R>({
+    required R Function(T data) success,
+    required R Function(String message, String? code, dynamic error) failure,
+  });
 }
 
+/// SUCCESS
 class Success<T> extends Result<T> {
   final T data;
 
   const Success(this.data);
 
   @override
+  R when<R>({
+    required R Function(T data) success,
+    required R Function(String message, String? code, dynamic error) failure,
+  }) {
+    return success(data);
+  }
+
+  @override
   List<Object?> get props => [data];
 }
 
+/// FAILURE
 class Failure<T> extends Result<T> {
   final String message;
   final String? code;
-  final dynamic exception;
+  final dynamic error;
 
   const Failure({
     required this.message,
     this.code,
-    this.exception,
+    this.error,
   });
 
   @override
-  List<Object?> get props => [message, code, exception];
+  R when<R>({
+    required R Function(T data) success,
+    required R Function(String message, String? code, dynamic error) failure,
+  }) {
+    return failure(message, code, error);
+  }
+
+  @override
+  List<Object?> get props => [message, code, error];
 }
 
-extension ResultExtension<T> on Result<T> {
-  R fold<R>(
-    R Function(Failure<T> failure) onFailure,
-    R Function(Success<T> success) onSuccess,
-  ) {
-    final value = this;
-
-    if (value is Success<T>) {
-      return onSuccess(value);
-    } else if (value is Failure<T>) {
-      return onFailure(value);
-    }
-
-    throw Exception('Unknown Result type');
+/// EXTENSION (compatibility layer)
+extension ResultX<T> on Result<T> {
+  T? getOrNull() {
+    final self = this;
+    if (self is Success<T>) return self.data;
+    return null;
   }
 
   bool get isSuccess => this is Success<T>;
   bool get isFailure => this is Failure<T>;
-
-  T? getOrNull() {
-    final value = this;
-    if (value is Success<T>) {
-      return value.data;
-    }
-    return null;
-  }
 }
